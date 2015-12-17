1082cdf9b51d7d8ecd59a0e1d24766edd62debf2
dm4000 <daniel@listretto.com>
2015-12-18 10:52:43 +1100

    Initial Commit for MVC

---
+++ script2.js
@@ -0,0 +1,99 @@
+var model = {
+  init: function () {},
+  cats: [
+    {
+      name: "Hank",
+      src: "img/hank.jpg",
+      counter: 0
+  },
+    {
+      name: "Sally",
+      src: "img/sally.jpg",
+      counter: 0
+  },
+    {
+      name: "Sam",
+      src: "img/sam.jpg",
+      counter: 0
+  },
+    {
+      name: "Tom",
+      src: "img/tom.jpg",
+      counter: 0
+
+  },
+    {
+      name: "Woolly",
+      src: "img/woolly.jpg",
+      counter: 0
+  },
+    {
+      name: "Tulip",
+      src: "img/tulip.jpg",
+      counter: 0
+  }
+  ]
+};
+
+var octopus = {
+  init: function () {
+    model.init();
+    view.init();
+  },
+
+  getCats: function () {
+    return model.cats;
+  }
+
+};
+
+var view = {
+  init: function () {
+
+    view.render();
+
+  },
+
+  render: function () {
+    var catList = document.getElementById("cat-list");
+    var display = document.getElementById("display");
+
+    octopus.getCats().forEach(function (cat) {
+      var li = document.createElement('li');
+
+      var counter = cat.counter;
+      var name = cat.name;
+      var src = cat.src;
+
+      li.innerHTML = cat.name;
+      catList.appendChild(li);
+
+      li.addEventListener('click', (function (counterCopy, nameCopy, srcCopy) {
+        return function () {
+          counterCopy += 1;
+          document.getElementById("counter").innerHTML = counterCopy;
+          document.getElementById("name").innerHTML = nameCopy;
+          document.getElementById("cat-image").src = srcCopy;
+
+
+
+          var reset = document.getElementById("reset");
+          reset.addEventListener('click', function () {
+            counterCopy = 0;
+            document.getElementById("counter").innerHTML = 0;
+            document.getElementById("name").innerHTML = "anything";
+            document.getElementById("cat-image").src = "";
+          });
+          // this needs to be inside the closure too, else the counters change to "0", but any subsequent clicks display a counter that is the stored value
+        };
+        // this means that each time the loop runs, this function DECLARATION is returned
+
+
+      })(counter, name, src));
+      // but this executes that function declaration immediately
+
+    });
+  }
+};
+
+octopus.init();
---
+++ script3.js
@@ -0,0 +1,85 @@
+var view = {
+  init: function () {
+
+    view.render();
+
+  },
+
+  render: function () {
+    var catList = document.getElementById("cat-list");
+    var display = document.getElementById("display");
+
+    octopus.getCats().forEach(function () {
+      var li = document.createElement('li');
+
+      var counter = model.cats[i].counter;
+      // there is no [i] 
+      // i is not defined
+      
+      // octupus.getCats() actually returns the "cat" property itself
+      // ie. we are given an array object to play with
+
+      var name = model.cats[i].name;
+      var src = model.cats[i].src;
+
+      li.innerHTML = i.name;
+      catList.appendChild(li);
+    });
+  }
+};
+
+// V2
+var view = {
+  init: function () {
+
+    view.render();
+
+  },
+
+  render: function () {
+    var catList = document.getElementById("cat-list");
+    var display = document.getElementById("display");
+
+    octopus.getCats().forEach(function (i) {
+      var li = document.createElement('li');
+
+      var counter = [i].counter;
+      // doesn't throw an error
+      // but "undefined" is printed onto the screen
+      // i represents each element in the array
+      // going numerically, because arrays are index: value
+      // where "value" is an object (with 3 cat properties)
+
+      var name = [i].name;
+      var src = [i].src;
+
+      li.innerHTML = i.name;
+      catList.appendChild(li);
+    });
+  }
+};
+
+// V3
+var view = {
+  init: function () {
+
+    view.render();
+
+  },
+
+  render: function () {
+    var catList = document.getElementById("cat-list");
+    var display = document.getElementById("display");
+
+    octopus.getCats().forEach(function (cat) {
+      var li = document.createElement('li');
+
+      var counter = cat.counter;
+      var name = cat.name;
+      var src = cat.src;
+
+      li.innerHTML = cat.name;
+      catList.appendChild(li);
+    });
+  }
+};
